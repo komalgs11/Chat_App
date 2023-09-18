6b5ac4117556b81a5812ab1230679d7993906da9
komalgs11 <komalpatil26.kp@gmail.com>
2023-09-19 01:02:29 +0200

    added firestore db functions

--- components/Chat.js
+++ components/Chat.js
@@ -1,41 +1,57 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+} from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import {
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params; //shows background color and name on chat pg
+const Chat = ({ route, navigation, db }) => {
+  const { name, color, userID } = route.params; //shows background color and name on chat pg
   const [messages, setMessages] = useState([]);
 
+  //the function adds new message to firestore
+  const addMessage = async (newMessages) => {
+    const newMessageRef = await addDoc(
+      collection(db, "messages"),
+      newMessages[0]
+    );
+    //if failed to add a new message
+    if (!newMessageRef.id) {
+      Alert.alert("Unable to add. Please try later");
+    }
+  };
+
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    const unsubMessages = onSnapshot(q, (docs) => {
+      let newMessages = [];
+      docs.forEach((doc) => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis()),
+        });
+      });
+      setMessages(newMessages);
+    });
+    return () => {
+      if (unsubMessages) unsubMessages();
+    };
   }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
-
-  const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
-  };
 
   const renderBubble = (props) => {
     return (
@@ -58,16 +74,14 @@ const Chat = ({ route, navigation }) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
-        onSend={(messages) => onSend(messages)}
+        onSend={(messages) => addMessage(messages)}
         user={{
-          _id: 1,
+          _id: userID,
+          name: name,
         }}
       />
-      {Platform.OS === "android" ? ( // to avoid keyboard hides the message input field in android
-        <KeyboardAvoidingView behavior="height" />
-      ) : null}
-
-      {Platform.OS === "ios" ? ( // to avoid keyboard hides the message input field in ios
+      {/* // to avoid keyboard hides the message input field in ios */}
+      {Platform.OS === "ios" ? (
         <KeyboardAvoidingView behavior="padding" />
       ) : null}
     </View>
